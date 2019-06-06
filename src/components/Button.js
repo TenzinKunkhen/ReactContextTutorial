93c848232f72331726784e3910ae8d9019d27b43
kunkhen <tenzin.kunkhen@ryerson.ca>
2019-06-05 22:30:02 -0400

    dynamic context reading added

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,10 +9,11 @@ class Button extends React.Component {
 
 
     render() {
-        console.log(this.context);
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+
         return (
             
-            <button className="ui button primary">Submit</button>
+            <button className="ui button primary">{text}</button>
         )
     }
 }
